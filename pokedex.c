283e1384d5cfd8c9c69917074a3a8b6d6ae2e2ec
takohhh <57528310+takohhh@users.noreply.github.com>
2021-11-17 16:59:31 +0000

    Add files via upload

--- pokedex.c
+++ pokedex.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include<stdlib.h>
 
-typedef struct Dex {
+typedef struct Pokedex {
    //pokedex struct
    //contains two pointers
-   //one to pokemon list
-   //one to player list
-} Dex;
+   //one to pokemon list head
+   //one to player list head
+} Pokedex;
 
 typedef struct PokemonNode {
+  //struct for each node in the pokemon list
+  //contains pokemon species name, type and primary ability
+  //stored as strings
   char name[50];
   char type[20];
   char ability[25];
@@ -17,44 +21,20 @@ typedef struct PokemonNode {
 } PokemonNode;
 
 typedef struct PlayerNode {
+  //struct for each node in the player list
+  //contains player name, number of pokemon owned and array of pokemon they own
+  //name and pokemon count stored as strings
+  //array is an array of pointers to pokemon owned
   char p_name[50];
   int owned[50];
   PokemonNode array[50];
 
 } PlayerNode;
 
-/*Functions
-PokemonNode* NewPokemonNode(name, type, ability) {
-  //creates a new node and returns a pointer to that node
-}
-
-void AddPokemonToList(*pokedex, name, type, ability) {
-  //checks to see if name already exists in the Pokemon list. 
-  //If it doesn’t it creates a new node and adds it to the list. It the Pokemon already exists it does nothing.
-}
-
-PokemonNode* FindPokemon(pokedex, name) {
-  //searches the Pokemon list for name. 
-  //If it finds name it returns a pointer to the name’s node otherwise it returns NULL.
-
-}
-*/
-/*
-Create a struct for each node in the list called PokemonNode. Each node in the list should store – the Pokemon’s name, 
-its type and its primary ability as strings. Assume each Pokemon has only a single type and a single ability.
-You should implement the basic functionality required for your list type, functions to: 
-create new nodes, add a node to the list, to find a specific node (by Pokemon name).
-Functions
--PokemonNode* NewPokemonNode(name, type, ability) – creates a new node and returns a pointer to that node
--void AddPokemonToList(*pokedex, name, type, ability) – checks to see if name already exists in the Pokemon list. 
-If it doesn’t it creates a new node and adds it to the list. It the Pokemon already exists it does nothing.
--PokemonNode* FindPokemon(pokedex, name) searches the Pokemon list for name. 
-If it finds name it returns a pointer to the name’s node otherwise it returns NULL.
-*/
-
 int main(void) {
+  PokemonNode *head = NULL;
+  
   /*
-  Book book1;
   Book *book1_ptr;
   book1_ptr = &book1;
 
@@ -69,3 +49,74 @@ int main(void) {
   printf("Book ID : %d\n", book1_ptr->book_id);
 */
 }
+
+//Functions
+PokemonNode* NewPokemonNode(name, type, ability) {
+  //creates a new node and returns a pointer to that node
+  PokemonNode *new_node = NULL;
+  new_node = malloc(sizeof(PokemonNode));
+
+  if (new_node != NULL)
+  {
+    new_node->name=name;
+    new_node->type=type;
+    new_node->ability=ability;
+    new_node->next=NULL;
+  }
+  return new_node;
+}
+
+void AddPokemonToList(*pokedex, name, type, ability) {
+  //checks to see if name already exists in the Pokemon list. 
+  //If it doesn’t it creates a new node and adds it to the list. It the Pokemon already exists it does nothing.
+
+  //checks to see if name already exists
+  if (FindPokemon(pokedex, name)==NULL) 
+  {
+    //if pokemon name not found then it is added to list
+    //creates new node using NewPokemonNode(name, type, ability) function
+    PokemonNode *temp = NewPokemonNode(name, type, ability);
+  }
+  else
+  {
+    //if pokemon name is already found in the list
+    //nothing happens and the pokemon isn't added again
+    printf("Pokemon already exists in list.")
+  }
+
+  //end AddPokemonToList() function
+}
+
+PokemonNode* FindPokemon(pokedex, name) {
+  //searches the Pokemon list for name. 
+  //If it finds name it returns a pointer to the name’s node otherwise it returns NULL.
+  PokemonNode *temp = head;  // always work with a copy of head!
+  
+  //check if current entry is the pokemon we are searching for
+  if (temp->name == name)
+    {
+      return temp;
+    }
+
+  //if the next entry is NULL then temp is the final item in the list
+  while (temp->next != NULL)
+  {
+    //if the next entry is not NULL, move on to the next node
+    temp = temp->next;
+    //check if current entry is the pokemon we are searching for
+    if (temp->name == name)
+    {
+      return temp;
+    }
+  }
+  return NULL;
+}
+//
+/*
+Functions
+-PokemonNode* NewPokemonNode(name, type, ability) – creates a new node and returns a pointer to that node
+-void AddPokemonToList(*pokedex, name, type, ability) – checks to see if name already exists in the Pokemon list. 
+If it doesn’t it creates a new node and adds it to the list. It the Pokemon already exists it does nothing.
+-PokemonNode* FindPokemon(pokedex, name) searches the Pokemon list for name. 
+If it finds name it returns a pointer to the name’s node otherwise it returns NULL.
+*/
